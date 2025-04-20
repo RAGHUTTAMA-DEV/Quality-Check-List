5f344fa29ae64bb72cd66adecd886a2718d806df
Raghuttama K G <raghuttama01@gmail.com>
2025-04-20 19:06:03 +0530

    Completed the main logic of CheckListController

--- Backend/controllers/ChecklistController.js
+++ Backend/controllers/ChecklistController.js
@@ -1,13 +1,15 @@
 import CheckListModel from "../models/CheckList.js";
 export async function CreateCheckList(req,res){
    try{
-     const {content,isChecked,coments,Image}=req.body;
+     const {content,isChecked,coments,Image,checkedBy}=req.body;
 
      await CheckListModel.create({
         content,
         isChecked,
-        comments
+        coments,checkedBy,Image
      });
+
+     res.json({message:"CheckList Created"});
 
    }catch(err){
       console.log(err);
@@ -15,18 +17,54 @@ export async function CreateCheckList(req,res){
    }
 }
 
-export function GetAllCheckList(req,res){
+export async  function GetAllCheckList(req,res){
+   try{
+    const checkList=await CheckListModel.find();
+    res.json({checkList,message:"CheckList Found"});
 
+   }catch(err){
+     console.log(err);
+      res.json({message:"Erroar"});
+   }
 }
 
-export function GetCheckList(req,res){
-   
+export async function GetCheckList(req,res){
+      try{
+        const checkList=await CheckListModel.findById(req.params.id);
+        res.json({checkList,message:"CheckList Found"});
+
+
+      }catch(err){
+         console.log(err);
+        res.json({message:"Erroar"});
+      }
 }
 
-export function UpdateCheckList(req,res){
+export async function UpdateCheckList(req,res){
+   try{
+    const {content,isChecked,coments,Image,checkedBy}=req.body;
+    const checkList=await CheckListModel.findByIdAndUpdate(req.params.id,{
+        content,
+        isChecked,
+        coments,checkedBy,Image
+    });
+    res.json({checkList,message:"CheckList Updated"});
 
+
+   }catch(err){
+     console.log(err);
+      res.json({message:"Erroar"});
+   }
 }
 
-export function DeleteCheckList(req,res){ 
+export async function DeleteCheckList(req,res){ 
+    try{ 
+        const checkList=await CheckListModel.findByIdAndDelete(req.params.id);
+        res.json({checkList,message:"CheckList Deleted"});
 
+
+    }catch(err){
+        console.log(err);
+        res.json({message:"Erroar"});
+    }
 }
