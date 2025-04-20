de6ca4e63a8bda0d433f146d993cb071c4724957
Raghuttama K G <raghuttama01@gmail.com>
2025-04-20 17:36:32 +0530

    Main controller changes

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 async function ConnectDb(){
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("Database Connectedon nothin");  
+    console.log("Database Connectedon");  
 }
 
 export default ConnectDb;   
