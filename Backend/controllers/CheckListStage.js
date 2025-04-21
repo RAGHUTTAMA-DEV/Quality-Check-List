3d0b58b6969cd059054868d6c0a446335757de72
Raghuttama K G <raghuttama01@gmail.com>
2025-04-21 18:08:21 +0530

    Added the main Comtroller Logic to CheckListItem model

--- Backend/controllers/CheckListStage.js
+++ Backend/controllers/CheckListStage.js
@@ -1,19 +1,61 @@
 import CheckListStage from "../models/CheckListStage";
 
 async function PostCheckListItem(req,res){
-   const 
+   try{
+    const {title,items,stage}=req.body;
+    await CheckListStage.create({title,items,stage});
+    res.status(201).json({message:"CheckListStage Created"});
+   }catch(err){
+     console.log(err);
+     res.status(500).json({message:"Internal Server Error"});   
+   }
 }
 
 async function UpdateCheckListItem(req,res){
+   try{
+      const {id}=req.params.id;
+      const updateItems=req.body;
+      await CheckListStage.findByIdAndUpdate(id,{
+        $set:updateItems
+      })
 
+      res.status(200).json({message:"CheckListStage Updated"});
+
+   }catch(err){
+     console.log(err);
+     res.status(500).json({message:"Internal Server Error"});
+   }
 }
 
 async function DeleteCheckListItem(req,res){
+    try{
+        const {id}=req.params.id;
+        await CheckListStage.findByIdAndDelete(id);
 
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:"Internal Server Error"});
+    }
 }
 
 async function MarkChecListItem(req,res){
+    try{
+        const {itemsId}=req.params.itemsId;
+        const {mark}=req.body;
+        await CheckListStage.findByIdAndUpdate(itemsId,{
+            $set:{
+                mark
+            }
+        })
+        res.status(200).json({message:"CheckListStage Updated"});
+        
 
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:"Internal Server Error"});
+    }
 }
 
 export {PostCheckListItem,UpdateCheckListItem,DeleteCheckListItem,MarkChecListItem};
