84e2f374cd42450fb8ba683ddd7207406f489dc5
Raghuttama K G <raghuttama01@gmail.com>
2025-04-27 10:25:50 +0530

    Trying to delop the backend and fixed the package.json in backend

--- Frontend/src/pages/items/itemsGet.tsx
+++ Frontend/src/pages/items/itemsGet.tsx
@@ -73,7 +73,7 @@ export default function ItemsGet() {
                 </motion.div>
 
                 <motion.div>
-                     MarkCheckListItem
+                     
                 </motion.div>
 
                 <motion.div>
